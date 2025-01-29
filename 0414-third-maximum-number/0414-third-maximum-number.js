ee012bb87b14e28c562c5297b5a06c316e9cc884
Sourav-PP <souravpp969@gmail.com>
2025-01-29 12:50:55 +0530

    Time: 4 ms (57.06%), Space: 51.3 MB (52.15%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.js
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -6,7 +6,7 @@ var thirdMax = function(nums) {
     let arr = nums.sort((a,b) => b-a)
     let final = [...new Set(arr)]
 
-    if(final[2]) {
+    if(final.length >= 3) {
         return final[2]
     }else {
         return final[0]
