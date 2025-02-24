e32d8a4ffd7df2d23bd2ae2eec2722e7b66b748d
Sourav-PP <souravpp969@gmail.com>
2025-02-24 19:20:12 +0530

    Time: 0 ms (100%), Space: 58.2 MB (5.5%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    while(n > 1) {
+        n = n / 4
+    }
+    if(n == 1) {
+        return true
+    }else{
+        return false
+    }
+};
