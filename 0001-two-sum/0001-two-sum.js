86bcf05963e11ac70ebeed19f3da9c23bc4e410a
Sourav-PP <souravpp969@gmail.com>
2025-03-01 22:03:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,12 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  for(let i=0;i<nums.length; i++){
-    for(let j=i+1;j<nums.length; j++){
-        if(nums[i]+nums[j]==target){
-            return [i,j]
+   let map = new Map()
+
+    for(let i=0; i<arr.length; i++) {
+        let match = target - arr[i]
+        if(map.has(match)) {
+            return [map.get(match), i]
+        }else{
+            map.set(arr[i], i)
         }
     }
-  }
-    
 };
