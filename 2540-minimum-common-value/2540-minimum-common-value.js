a9102cd0c5d156f99d2df205a72ab160491ed106
Sourav-PP <souravpp969@gmail.com>
2025-03-11 07:24:49 +0530

    Time: 21 ms (14.81%), Space: 74.8 MB (10.33%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.js
+++ 2540-minimum-common-value/2540-minimum-common-value.js
@@ -5,12 +5,12 @@
  */
 var getCommon = function(nums1, nums2) {
     let seen = new Set(nums1)
-    let common = []
+    let common = Infinity
     for(let num of nums2) {
         if(seen.has(num)) {
-            common.push(num)
+            common = Math.min(common, num)
             seen.delete(num)
         }
     }
-    return Math.min(...common)
+    return common === Infinity ? -1 : common
 };
