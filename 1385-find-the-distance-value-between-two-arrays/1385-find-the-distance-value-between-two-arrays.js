9c17982b04d181847a626f281c8e628e8edbdd7b
Sourav-PP <souravpp969@gmail.com>
2025-03-13 19:55:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.js
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.js
@@ -5,18 +5,18 @@
  * @return {number}
  */
 var findTheDistanceValue = function(arr1, arr2, d) {
-    let count=0;
-    for(let i=0;i<arr1.length;i++){
-        let flag=false;
-        for(let j=0;j<arr2.length;j++){
-            if(Math.abs(arr1[i]-arr2[j])<=d){
-                flag=true;
-                break;
+    let count = 0
+    for(let i=0; i<arrr1.length; i++) {
+        let invalid = false
+        for(let j=0; j<arr2.length; j++) {
+            if(Math.abs(arr[i] - arr[j]) <= d) {
+                invalid = true
+                break
             }
         }
-        if(flag==false){
-            count+=1;
+        if(invalid === true) {
+            count ++
         }
     }
-    return count;
+    return count
 };
