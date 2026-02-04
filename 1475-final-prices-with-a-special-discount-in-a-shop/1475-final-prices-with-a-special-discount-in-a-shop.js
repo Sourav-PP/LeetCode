7ffdca32759f6c08ff0fdc50f05a46b913e3aa4d
Sourav-PP <souravpp969@gmail.com>
2026-02-04 14:41:16 +0530

    Time: 0 ms (100%), Space: 57.7 MB (55.95%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number[]}
+ */
+var finalPrices = function(prices) {
+    let answer = []
+    for(let i=0; i<prices.length; i++) {
+        let discount = 0
+        for(let j=i+1; j<prices.length; j++) {
+            if(prices[i] >= prices[j]) {
+                discount = prices[j]
+                break
+            }
+        }
+        answer[i]= prices[i] - discount
+    }
+    return answer
+};
